db3274d93f5ea19f0e865cf79af162bcc46f4886
Huang <lhuang28@swarmctrl-12lin.ee.e.uh.edu>
2019-01-31 11:51:13 -0600

    data cleaning

--- examples/Hal_mlp.py
+++ examples/Hal_mlp.py
@@ -6,6 +6,7 @@ dir =  os.getcwd()
 # ====================================
 import matplotlib.pyplot as plt
 import numpy, pandas
+from io import StringIO
 
 # from keras.models import Sequential
 # from keras.layers import Dense
@@ -15,19 +16,39 @@ numpy.random.seed(7)
 
 # load the datasetd
 # dataframe = pandas.read_csv(os.path.join(dir,'data','Volve production data.csv'), usecols=[1], engine='python')
-dataframe = pandas.read_csv(os.path.join(dir,'data','Volve production data.csv'), index_col=["DATEPRD", "WELL_BORE_CODE"], 
-            usecols=["WELL_BORE_CODE", "ON_STREAM_HRS", "AVG_DOWNHOLE_PRESSURE", "AVG_DOWNHOLE_TEMPERATURE", 
-                     "AVG_DP_TUBING", "AVG_ANNULUS_PRESS", "AVG_CHOKE_SIZE_P", "AVG_WHP_P", "AVG_WHT_P",
-                      "DP_CHOKE_SIZE", "BORE_OIL_VOL", "FLOW_KIND"], parse_dates=["DATEPRD"], engine='python')
+# dataframe = pandas.read_csv(StringIO(os.path.join(dir,'data','Volve production data.csv')),header=0, index_col=['DATEPRD'],
+#             usecols=['WELL_BORE_CODE', 'ON_STREAM_HRS', 'AVG_DOWNHOLE_PRESSURE', 'AVG_DOWNHOLE_TEMPERATURE',
+#                      'AVG_DP_TUBING', 'AVG_ANNULUS_PRESS', 'AVG_CHOKE_SIZE_P', 'AVG_WHP_P', 'AVG_WHT_P',
+#                       'DP_CHOKE_SIZE', 'BORE_OIL_VOL', 'FLOW_KIND'], names=['WELL_BORE_CODE', 'ON_STREAM_HRS', 'AVG_DOWNHOLE_PRESSURE', 'AVG_DOWNHOLE_TEMPERATURE',
+#                      'AVG_DP_TUBING', 'AVG_ANNULUS_PRESS', 'AVG_CHOKE_SIZE_P', 'AVG_WHP_P', 'AVG_WHT_P',
+#                       'DP_CHOKE_SIZE', 'BORE_OIL_VOL', 'FLOW_KIND', 'DATEPRD'], parse_dates=['DATEPRD'], engine='c')
 
-# dataset = dataframe.values
-# dataset = dataset.astype('float32')
+
+dataframe = pandas.read_csv(os.path.join(dir,'data','VolveData.csv'), header=0,
+            usecols=['ON_STREAM_HRS', 'AVG_DOWNHOLE_PRESSURE', 'AVG_DOWNHOLE_TEMPERATURE',
+                     'AVG_DP_TUBING', 'AVG_ANNULUS_PRESS', 'AVG_CHOKE_SIZE_P', 'AVG_WHP_P', 'AVG_WHT_P',
+                      'DP_CHOKE_SIZE', 'BORE_OIL_VOL', 'FLOW_KIND'], engine='python')
+
+
+df = dataframe[dataframe['FLOW_KIND'] == 'production']
+df = df.drop(['FLOW_KIND'],axis=1)
+df = df.fillna(0)
+df = df.replace({',': ''}, regex=True)
+df_y = df['BORE_OIL_VOL']
+y = df_y.values
+df_X = df.drop(['BORE_OIL_VOL'], axis=1)
+X = df_X.values
+
+import numpy as np
+dataset = np.concatenate([np.asarray(X),np.asarray([y]).T], axis=1)
+dataset = dataset.astype('float32')
+
 
 # # split into train and test sets
-# train_size = int(len(dataset) * 0.67)
-# test_size = len(dataset) - train_size
-# train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
-# print(len(train), len(test))
+train_size = int(len(dataset) * 0.8)
+test_size = len(dataset) - train_size
+train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
+print(len(train), len(test))
 
 
 # # convert an array of values into a dataset matrix
